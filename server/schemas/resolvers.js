e2fa4dbe263b5043df2c9d64c321ff1ec3d828da
Brayden <brayburl224@gmail.com>
2023-01-21 20:35:21 -0500

    drafted out first query to find a single user

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+const { User, Book } = require('../models');
+
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        user: async (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return User.find(params);
+        }
+    }
+}
