e62ceffe7b9380c959354e9e93df9721b885f27f
Brayden <brayburl224@gmail.com>
2023-01-21 23:19:46 -0500

    fixed up query in resolver.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,24 @@
-const { User, Book } = require('../models');
+const { User } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        user: async (parent, { _id }) => {
-            const params = _id ? { _id } : {};
-            return User.find(params);
+        me: async (parent, _id , context) => {
+            if (context.user) {
+            const searchId = await User.findOne({ _id: context.user._id })
+            .select("-__v -password");
+            return searchId;
+            }
+        throw new AuthenticationError('You need to be logged in!');
         }
+    },
+    
+    Mutation: {
+
     }
 }
+
+
 
 module.exports = resolvers;
