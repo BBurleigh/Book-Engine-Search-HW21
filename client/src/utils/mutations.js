cfd694526f72f25991b3b9bde34593425a705dbc
Brayden <brayburl224@gmail.com>
2023-01-22 19:06:24 -0500

    drafted out SAVE_BOOK and DELETE_BOOK mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,13 +13,53 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
 `;
 
 export const SAVE_BOOK = gql`
-
+    mutation saveBook($input: savedBook!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                title
+                authors
+                description
+                bookId
+                image
+                link
+            }
+        }
+    }
 `;
 
 export const DELETE_BOOK = gql`
-
+    mutation deleteBook($bookId: ID!) {
+        deleteBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                title
+                authors
+                description
+                bookId
+                image
+                link
+            }
+        }
+    }
 `;
