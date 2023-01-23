91e9f50caf90b952782932df56455b95f6503cc7
Brayden <brayburl224@gmail.com>
2023-01-23 09:37:27 -0500

    pushing most recent fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
@@ -22,9 +28,7 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
+
 
 function App() {
   return (
